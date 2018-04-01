706989270e142192c6d956e6515d1c042030ecac
Matheus Ribeiro D'Azevedo Lopes <math222ribeiro@hotmail.com>
2018-04-01 17:57:58 -0300

    Ar contents

--- MRLab.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/Contents.swift
+++ MRLab.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/Contents.swift
@@ -1,5 +1,26 @@
-
-import PlaygroundSupport
-
-print("Hello World")
-print(PlaygroundKeyValueStore.current["robot"])
+/*:
+ **Experiment:** Create your own AR Scene on the liveView with your new robot.
+ 
+ And now the magic! 🎩🌟
+ 
+ With the power of AR we can give life to your robot.
+ 
+ * callout(Live View):
+ 
+    - See the tracking information (top left view).
+    - Reset the tracking (top left button).
+    - Place objects on the scene with the buttons with the label "AR".
+ 
+ Now in order to create your scene
+ 
+ * Callout(Instructions):
+    - First move around to detect planes
+    - When a grey plane appears on the screen you can tap on it to place your robot
+    - After placing your robot you can chose another object to place on the scene by chosing one of the AR labeled buttons.
+    - The bottom left button place tables of the lab. there are three available tables.
+ 
+ 
+ **Create by Matheus Ribeiro D'Azevedo Lopes for WWDC18**
+ */
+// Have Fun!
+// Thank You!
