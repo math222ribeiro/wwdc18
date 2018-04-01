eb3846ceadf71a1aee3e885ad7b4e58e58552d82
Matheus Ribeiro D'Azevedo Lopes <math222ribeiro@hotmail.com>
2018-04-01 19:09:00 -0300

    minor fixes

--- MRLab.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/Contents.swift
+++ MRLab.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/Contents.swift
@@ -16,10 +16,11 @@
  * Callout(Instructions):
     - First move around to detect planes
     - When a grey plane appears on the screen you can tap on it to place your robot
-    - After placing your robot you can chose another object to place on the scene by chosing one of the AR labeled buttons.
+    - After placing your robot you can chose another object to place on the scene by chosing one of the AR labeled buttons, and then tapping on the screen.
     - The bottom left button place tables of the lab. there are three available tables.
     - If you select a button of an object that is already on the scene, when tapping on the scene you change its position.
  
+ Oh, I almost forgot to say. I loooove to dance 😄. You will see. Bye!
  
  **Create by Matheus Ribeiro D'Azevedo Lopes for WWDC18**
  */
